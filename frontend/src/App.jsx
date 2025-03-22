e1b42f0d13406afb462eb4dabe4f3cf51dcd0ab9
IRFAN AHMED <Mrlagend09@gmail.com>
2025-03-22 14:57:32 +0600

    edit commit
    edit commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import {Routes,Route} from 'react-router-dom'
-import Home from './pages/Home'
-import Collection from './pages/Collection'
-import About from './pages/About'
-import Contact from './pages/contact'
-import Login from './pages/Login'
-import Product from './pages/Product'
-import Cart from './pages/Cart'
-import PlaceOrder from './pages/PlaceOrder'
-import Order from './pages/Order'
-import Navbar from './components/Navbar'
-import SearchBar from './components/SearchBar'
-import Footer from './components/Footer'
+import Home from './pages/Home.jsx'
+import Collection from './pages/Collection.jsx'
+import About from './pages/About.jsx'
+import Contact from './pages/Contact.jsx'
+import Login from './pages/Login.jsx'
+import Product from './pages/Product.jsx'
+import Cart from './pages/Cart.jsx'
+import PlaceOrder from './pages/PlaceOrder.jsx'
+import Order from './pages/Order.jsx'
+import Navbar from './components/Navbar.jsx'
+import SearchBar from './components/SearchBar.jsx'
+import Footer from './components/Footer.jsx'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Verify from './pages/verify'
+import Verify from './pages/Verify.jsx'
 
 const App = () => {
   return (
